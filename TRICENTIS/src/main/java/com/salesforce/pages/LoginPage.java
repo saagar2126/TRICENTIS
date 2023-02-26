59cd734d31e407394ae5d01861a08b1f464da707
saagar <saagar@192.168.1.43>
2023-02-26 20:46:05 +0530

    First Commit

---
+++ TRICENTIS/src/main/java/com/salesforce/pages/LoginPage.java
@@ -0,0 +1,31 @@
+package com.salesforce.pages;
+
+import com.framework.selenium.api.design.Locators;
+import com.framework.testng.api.base.ProjectSpecificMethods;
+
+public class LoginPage extends ProjectSpecificMethods{
+	
+	public LoginPage clickLoginURL() {
+		click(locateElement(Locators.CLASS_NAME, "ico-login"));
+		return this;
+	}
+	
+	public LoginPage enterUsername(String data) {
+		clearAndType(locateElement(Locators.ID, "Email"), data);
+		reportStep(data+" entered successfully","pass");
+		return this;
+	}
+	
+	public LoginPage enterPassword(String data) {
+		clearAndType(locateElement(Locators.ID, "Password"), data);
+		reportStep(data+" entered successfully","pass");
+		return this;
+	}
+	
+	public LoginPage clickLogin() {
+		click(locateElement(Locators.XPATH, "//*[@value='Log in']"));
+		reportStep("Login button clicked successfully", "pass");
+		return this;
+	}
+
+}
